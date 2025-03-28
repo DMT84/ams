3e97d21d6f47c646b7e7688378898c2a857fefb4
DMT84 <dimitri.botella84@gmail.com>
2025-03-28 15:37:56 +0100

    Update collect.py

--- collect.py
+++ collect.py
@@ -5,12 +5,10 @@ import re
 import os
 import shutil
 
-# Fonction pour extraire un nombre avec une expression régulière
 def extract_number(output, regex_pattern):
     match = re.search(regex_pattern, output)
     return float(match.group(1)) if match else 0
 
-# Fonction pour effectuer la collecte des données et les insérer dans la base de données
 def collect_data():
     base_path = "/home/cristiano/projet/ams"  # Ton chemin absolu
 
@@ -49,27 +47,23 @@ def collect_data():
     conn.close()
     print("Toutes les données ont été insérées avec succès !")
 
-# Fonction pour effectuer une sauvegarde de la base de données
 def backup_database():
     base_path = "/home/cristiano/projet/ams"
     db_path = os.path.join(base_path, "monitoring.db")
     backup_path = os.path.join(base_path, "monitoring_backup.db")
 
     try:
-        # Copie la base de données vers un fichier de sauvegarde
         shutil.copy2(db_path, backup_path)
         print(f"Base de données sauvegardée sous {backup_path}")
     except Exception as e:
         print(f"Erreur lors de la sauvegarde de la base de données: {e}")
 
-# Fonction pour restaurer la base de données à partir d'une sauvegarde
 def restore_database():
     base_path = "/home/cristiano/projet/ams"
     db_path = os.path.join(base_path, "monitoring.db")
     backup_path = os.path.join(base_path, "monitoring_backup.db")
 
     try:
-        # Remplace la base de données actuelle par la sauvegarde
         shutil.copy2(backup_path, db_path)
         print(f"Base de données restaurée à partir de {backup_path}")
     except Exception as e:
