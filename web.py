213edc15331e162c8cd0aed36d5eb96ff7bc7ff0
DMT84 <dimitri.botella84@gmail.com>
2025-04-10 08:49:07 +0200

    Create web.py

---
+++ web.py
@@ -0,0 +1,13 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def afficher_graphiques():
+    # Renvoie le fichier HTML généré dans 'static/'
+    return send_from_directory('graphs', 'graphiques_sondes.html')
+
+if __name__ == '__main__':
+    # Active le mode débogage pour avoir plus de détails sur les erreurs
+    app.run(host='0.0.0.0', port=7777, debug=True)
